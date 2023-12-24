b6cafe59193ce491aa97a0101f7b1be90f75b76c
el-barae <elbarae.akachar@gmail.com>
2023-12-24 12:02:43 +0100

    get all permissions

--- src/main/java/com/project/configuration/SecurityConfig.java
+++ src/main/java/com/project/configuration/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
       .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
       .authorizeHttpRequests(auth -> auth.requestMatchers("/api/v1/auth/**").permitAll()
     		  
-      //.requestMatchers("/api/v1/**").permitAll()
+      .requestMatchers("/api/v1/**").permitAll()
       
       .anyRequest().authenticated())
       .authenticationProvider(authenticationProvider)
